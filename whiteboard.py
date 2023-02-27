cb548149565933eec6e2872b6bb1b2284f413e83
Brian <bstanton773@gmail.com>
2023-02-27 13:07:02 -0600

    Morning lecture + whiteboard

---
+++ whiteboard.py
@@ -0,0 +1,11 @@
+# Given a list of integers, write a function determining whether the sum of its elements is odd or even.
+
+# Return your answer as a string matching "odd" or "even".
+
+# If the input array is empty, consider it as [0](array with a zero).
+
+def solution(list_nums):
+    # write your code here
+    return 'even' if sum(list_nums) % 2 == 0 else 'odd'
+
+# print(solution([0, 1, 4]))
